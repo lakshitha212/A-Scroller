406188b9f2fe93698dfdd3f8d4e30d6b0f190dd6
lakshitha212 <chameera.lakshitha212@gmail.com>
2015-12-29 10:34:10 +0530

    set angular templating

--- js/scroller.js
+++ js/scroller.js
@@ -1,3 +1,30 @@
 /**
  * Created by Lucky212 on 12/28/2015.
  */
+var scrollerApp = angular.module('scrollerApp', ['ngRoute']);
+scrollerApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: './pages/home.html',
+            controller: 'mainController'
+        })
+        .when('/about', {
+            templateUrl: './pages/about.html',
+            controller: 'aboutController'
+        })
+        .when('/contact', {
+            templateUrl: './pages/contact.html',
+            controller: 'contactController'
+        });
+});
+scrollerApp.controller('mainController', function ($scope) {
+    $scope.message = 'Home Page';
+});
+
+scrollerApp.controller('aboutController', function ($scope) {
+    $scope.message = 'Aboout Page';
+});
+
+scrollerApp.controller('contactController', function ($scope) {
+    $scope.message = 'Contact Page.';
+});
